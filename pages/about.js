6b16a9101416960757fb336548dfd8961da63753
emericgautier <emericgtr@gmail.com>
2022-10-11 09:11:47 +0200

    Create portfolio and about pages

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const about = () => {
+  return (
+    <div>
+      <h2>ABOUT</h2>
+    </div>
+  );
+};
+
+export default about;
+<h2>ABOUT</h2>;
---
+++ pages/portfolio.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const portfolio = () => {
+  return (
+    <div>
+      <h2>PORTFOLIO</h2>
+    </div>
+  );
+};
+
+export default portfolio;
