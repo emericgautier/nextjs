99838ffbb6e5626d34a358e7914daee6ab5f86af
emericgautier <emericgtr@gmail.com>
2022-10-11 13:56:40 +0200

    Create Meta components for pages

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,11 +1,18 @@
 import React from "react";
+import Meta from "../components/Meta";
 
 const portfolio = () => {
   return (
     <div>
+      <Meta
+        title={"Portfolio dev, les tous derniers projets réalisés"}
+        description={"Suite de 70  mots"}
+      />
       <h2>PORTFOLIO</h2>
     </div>
   );
 };
+
+// Executer le code coté serveur
 
 export default portfolio;
